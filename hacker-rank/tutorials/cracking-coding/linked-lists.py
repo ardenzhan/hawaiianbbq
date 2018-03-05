097bd3fc15a85f8ab73f680b3129290d3bab6f6d
Arden Zhan <arden@protonmail.ch>
2018-03-04 21:42:40 -0600

    implemented linked list

---
+++ hacker-rank/tutorials/cracking-coding/linked-lists.py
@@ -0,0 +1,65 @@
+# https://www.hackerrank.com/topics/linked-lists
+
+class Node:
+  def __init__(self, data = None, next_node = None):
+    self.data = data
+    self.next = next_node
+
+  def append(self, data):
+    current = self
+
+    while current.next is not None:
+      current = current.next
+
+    current.next = Node(data)
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+  
+  def append(self, data):
+    if self.head is None: 
+      self.head = Node(data)
+      return
+
+    # go to end of list where next is None, then append new Node
+    current = self.head
+
+    while current.next is not None:
+      current = current.next
+    current.next = Node(data)
+
+  def prepend(self, data):
+    newHead = Node(data)
+    newHead.next = self.head
+    self.head = newHead
+
+  def deleteValue(self, data):
+    if self.head is None: 
+      return
+    if self.head.data is data:
+      self.head = self.head.next
+      return
+
+    current = self.head
+    while current.next is not None:
+      if current.next.data is data:
+        current.next = current.next.next #skip the next value
+        return
+      current = current.next
+
+  def printList(self):
+    current = self.head
+    while current is not None:
+      print current.data,
+      current = current.next
+    print
+
+test = LinkedList()
+test.append(4)
+test.append(5)
+test.append(6)
+test.prepend(1)
+test.printList()
+test.deleteValue(4)
+test.printList()
