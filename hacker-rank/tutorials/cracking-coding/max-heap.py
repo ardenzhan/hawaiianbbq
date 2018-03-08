228c2bc4021404effb0019f5b25eff8f4ac812f5
Arden Zhan <arden@protonmail.ch>
2018-03-08 14:47:20 -0600

    implemented max heap

--- hacker-rank/tutorials/cracking-coding/max-heap.py
+++ hacker-rank/tutorials/cracking-coding/max-heap.py
@@ -1,6 +1,5 @@
-class MinHeap:
+class MaxHeap:
   def __init__(self):
-    self.size = 0
     self.items = []
 
   def __repr__(self):
@@ -10,32 +9,31 @@ class MinHeap:
   def getRightChildIndex(self, parentIndex): return 2 * parentIndex + 2
   def getParentIndex(self, childIndex): return (childIndex - 1) / 2
 
-  def hasLeftChild(self, index): return self.getLeftChildIndex(index) < self.size
-  def hasRightChild(self, index): return self.getRightChildIndex(index) < self.size
+  def hasLeftChild(self, index): return self.getLeftChildIndex(index) < len(self.items)
+  def hasRightChild(self, index): return self.getRightChildIndex(index) < len(self.items)
   def hasParent(self, index): return self.getParentIndex(index) >= 0
 
   def leftChild(self, index): return self.items[self.getLeftChildIndex(index)]
   def rightChild(self, index): return self.items[self.getRightChildIndex(index)]
   def parent(self, index): return self.items[self.getParentIndex(index)]
 
-  def swap(self, indexOne, indexTwo):
-    self.items[indexOne], self.items[indexTwo] = self.items[indexTwo], self.items[indexOne]
+  def swap(self, indexA, indexB):
+    self.items[indexA], self.items[indexB] = self.items[indexB], self.items[indexA]
   
   def peek(self):
-    # return root, which will be minimum element of heap
-    if self.size is 0: return None
+    # return root, which will be max element of heap
+    if len(self.items) is 0: return None
     return self.items[0]
 
   def poll(self):
     """ 
-    retrieve (pop) root element of heap
-    swap last added element to the root, then Heapify down (reorder back down with bubble)
+    retrieve (pop) root (max) element of heap
+    move last added element to the root, then Heapify down (reorder back down with bubble)
     """
+    if len(self.items) is 0: return None
 
-    if self.size is 0: return None
     item = self.items[0]
     self.items[0] = self.items.pop()
-    self.size -= 1
     self.heapifyDown()
     return item
 
@@ -45,37 +43,38 @@ class MinHeap:
     Heapify up until in order
     """
     self.items.append(item)
-    self.size += 1
     self.heapifyUp()
 
   def heapifyDown(self):
-    # start with root, keep swapping down while children are smaller (out of order)
+    # start with root element, keep swapping element down while children are larger (out of order)
     index = 0
 
     # only needs to check left child, because no right if no left
     while self.hasLeftChild(index):
 
-      # set smallerChildIndex to smaller of left or right child
-      smallerChildIndex = self.getLeftChildIndex(index)
-      if self.hasRightChild(index) and self.rightChild(index) < self.leftChild(index):
-        smallerChildIndex = self.getRightChildIndex(index)
+      # set largerChildIndex to larger of left or right child
+      largerChildIndex = self.getLeftChildIndex(index)
+      if self.hasRightChild(index) and self.rightChild(index) > self.leftChild(index):
+        largerChildIndex = self.getRightChildIndex(index)
       
-      if self.items[index] < self.items[smallerChildIndex]: break
-      else: self.swap(index, smallerChildIndex)
+      if self.items[index] > self.items[largerChildIndex]: break
+      else: self.swap(index, largerChildIndex)
       
-      index = smallerChildIndex
+      index = largerChildIndex
 
   def heapifyUp(self):
-    # start with last added element then walk up while there's a larger parent (out of order)
-    index = self.size - 1
-    while self.hasParent(index) and self.parent(index) > self.items[index]:
+    # start with last added element then walk up while there's a smaller parent (out of order)
+    index = len(self.items) - 1
+    while self.hasParent(index) and self.parent(index) < self.items[index]:
       self.swap(self.getParentIndex(index), index)
       index = self.getParentIndex(index)
 
 
-test = MinHeap()
+test = MaxHeap()
 test.add(4)
+print test
 test.add(5)
+print test
 test.add(10)
 print test
 test.add(1)
